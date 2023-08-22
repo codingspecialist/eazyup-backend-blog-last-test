4e06d71c6b505499f5a6b5b16642209c4b255c9f
metacoding <getinthere@naver.com>
2023-08-22 14:32:52 +0900

    댓글삭제완료

--- src/main/java/shop/mtcoding/blogv2/reply/ReplyController.java
+++ src/main/java/shop/mtcoding/blogv2/reply/ReplyController.java
@@ -4,6 +4,8 @@ import javax.servlet.http.HttpSession;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -20,6 +22,21 @@ public class ReplyController {
 
     @Autowired
     private HttpSession session;
+
+    @DeleteMapping("/api/reply/{id}/delete")
+    public @ResponseBody ApiUtil<String> delete(@PathVariable Integer id){
+        // 1. 인증체크
+        User sessionUser = (User) session.getAttribute("sessionUser");
+        if(sessionUser == null){
+            throw new MyApiException("인증되지 않았습니다");
+        }
+
+        // 2. 핵심로직
+        replyService.댓글삭제(id, sessionUser.getId());
+
+        // 3. 응답
+        return new ApiUtil<String>(true, "댓글삭제 완료");
+    }
 
 
     @PostMapping("/api/reply/save")
--- src/main/java/shop/mtcoding/blogv2/reply/ReplyService.java
+++ src/main/java/shop/mtcoding/blogv2/reply/ReplyService.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import shop.mtcoding.blogv2._core.error.ex.MyApiException;
 import shop.mtcoding.blogv2._core.error.ex.MyException;
 import shop.mtcoding.blogv2.board.Board;
 import shop.mtcoding.blogv2.board.BoardRepository;
@@ -29,11 +30,27 @@ public class ReplyService {
         User user = User.builder().id(sessionId).build();
 
         Reply reply = Reply.builder()
-            .comment(saveDTO.getComment())
-            .board(board)
-            .user(user)
-            .build();
+                .comment(saveDTO.getComment())
+                .board(board)
+                .user(user)
+                .build();
         replyRepository.save(reply); // entity : Reply 객체
+    }
+
+    @Transactional
+    public void 댓글삭제(Integer id, Integer sessionUserId) {
+        Optional<Reply> replyOP = replyRepository.findById(id);
+
+        if (replyOP.isEmpty()) {
+            throw new MyApiException("삭제할 댓글이 없습니다");
+        }
+
+        Reply reply = replyOP.get();
+        if (reply.getUser().getId() != sessionUserId) {
+            throw new MyApiException("해당 댓글을 삭제할 권한이 없습니다");
+        }
+
+        replyRepository.deleteById(id);
     }
 
 }
