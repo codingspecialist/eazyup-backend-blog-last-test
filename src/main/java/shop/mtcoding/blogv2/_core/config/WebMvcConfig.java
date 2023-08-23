ee375b1c5a798e73ef4bbecfcb1c5343cf745739
metacoding <getinthere@naver.com>
2023-08-23 12:53:34 +0900

    사진 외부 경로 설정하기

---
+++ src/main/java/shop/mtcoding/blogv2/_core/config/WebMvcConfig.java
@@ -0,0 +1,23 @@
+package shop.mtcoding.blogv2._core.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.resource.PathResourceResolver;
+
+@Configuration
+public class WebMvcConfig implements WebMvcConfigurer{
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        WebMvcConfigurer.super.addResourceHandlers(registry);
+
+        registry.addResourceHandler("/images/**")
+            .addResourceLocations("file:"+"./images/")
+            .setCachePeriod(10) // 10 (초)
+            .resourceChain(true)
+            .addResolver(new PathResourceResolver());
+    }
+    
+    
+}
